197a05d9d839306c98ca489cfd29b310439e2793
David McCallum <mccallum@uoregon.edu>
2019-07-03 08:02:49 -0700

    clear
    clear

--- 15-35b5c9307a8ba075b003.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{253:function(n,w,o){}}]);
-//# sourceMappingURL=15-35b5c9307a8ba075b003.js.map
--- vendor/copy.js
+++
@@ -1,8 +0,0 @@
-// Copy to clipboard functionality
-if (document.querySelector(".pattern-code-copy-btn")) {
-	var clipboard = new Clipboard('.pattern-code-copy-btn');
-
-	clipboard.on('success', function(e) {
-	    e.trigger.textContent = 'Copied';
-	});
-}
